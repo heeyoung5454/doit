61193f4678f22158716a7ece94a5a8076d7dd2a7
heeyoung <heeyoung5454@naver.com>
2023-06-24 17:42:03 +0900

    불필요한 코드 정리(쿠키관련) & css 수정

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -13,7 +13,6 @@ import {
 import { isSameMonth, isSameDay, addDays } from "date-fns";
 import "../assets/main.scss";
 import axios from "axios";
-import { setCookie, getCookie } from "../utile/cookie";
 
 // reference site https://sennieworld.tistory.com/74
 
@@ -327,13 +326,6 @@ const Main = () => {
     }
   };
 
-  const removeCookie = (name) => {
-    setCookie("test2", "12345");
-    let test = getCookie(name);
-    console.log(getCookie("test2"));
-    console.log(test, "cookie");
-  };
-
   return (
     <div className="main">
       <div className="calendar">
@@ -357,8 +349,6 @@ const Main = () => {
         <button className="next" onClick={() => moveUrl("add")}>
           등록하기
         </button>
-
-        <div onClick={() => removeCookie("JSESSIONID")}>로그아웃</div>
       </div>
       <div className="friend">
         <FriendList />
